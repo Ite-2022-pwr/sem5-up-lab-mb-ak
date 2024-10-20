484774ebc7618bf33ad4dd3903287be19b323298
Artur Kręgiel <arkregiel@gmail.com>
2024-10-20 16:36:20 +0200

    buja

---
+++ Karta-dzwiekowa/Karta-dzwiekowa/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Karta_dzwiekowa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(312, 57);
+            button1.Margin = new Padding(5, 5, 5, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(141, 37);
+            button1.TabIndex = 0;
+            button1.Text = "Browse";
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(13, 62);
+            textBox1.Margin = new Padding(5, 5, 5, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(266, 27);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "File path";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(467, 409);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Form1";
+            Text = "Platform Invoke WinSound C#";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
